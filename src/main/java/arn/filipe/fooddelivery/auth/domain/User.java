677c2039d6161aa9bf3bbebab2a338c2ab76747a
filipearn <filipearn@yahoo.com.br>
2021-09-02 00:01:53 -0300

    Changing code to save bcrypt password on database

---
+++ src/main/java/arn/filipe/fooddelivery/auth/domain/User.java
@@ -0,0 +1,32 @@
+package arn.filipe.fooddelivery.auth.domain;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.time.OffsetDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Data
+public class User {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+
+}
