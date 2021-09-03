d3b918338ceaaa49c72f418d072d418ad88912d3
filipearn <filipearn@yahoo.com.br>
2021-09-02 23:04:47 -0300

    Adding authorities param.

--- src/main/java/arn/filipe/fooddelivery/auth/core/AuthUser.java
+++ src/main/java/arn/filipe/fooddelivery/auth/core/AuthUser.java
@@ -1,9 +1,11 @@
 package arn.filipe.fooddelivery.auth.core;
 
 import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 
 
+import java.util.Collection;
 import java.util.Collections;
 
 @Getter
@@ -13,8 +15,8 @@ public class AuthUser extends User {
     private Long userId;
     private String fullName;
 
-    public AuthUser(arn.filipe.fooddelivery.auth.domain.User user){
-        super(user.getEmail(), user.getPassword(), Collections.EMPTY_LIST);
+    public AuthUser(arn.filipe.fooddelivery.auth.domain.User user, Collection<? extends GrantedAuthority> authorities){
+        super(user.getEmail(), user.getPassword(), authorities);
 
         this.userId = user.getId();
         this.fullName = user.getName();
